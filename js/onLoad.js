f5385fd314f95379ea0d604457ed61a5df069039
Raphael <raphael@webmanifestation.com>
2015-07-27 02:25:29 +0530

    Setup
    Index html file, style sheet, jQuery and onload js file setup

---
+++ js/onLoad.js
@@ -0,0 +1,23 @@
+jQuery(function($) {
+
+//Function to set element height to window height.
+$.fn.setHightFull = function() {
+
+	return this.each(function() {
+
+		var windowCurrentHeight = $(window).height();
+		
+		$(this).css('height', windowCurrentHeight + 'px');
+
+	});
+};
+
+
+$(document).ready(function() {
+
+	$('.block').setHightFull();
+
+});
+
+
+});
